e85b0f27d720edb2f39b1a74a23e4f3b97467afa
JRodEud <jrod_eudavepat@hotmail.com>
2020-08-17 15:09:30 -0500

    Inicial Project Add

---
+++ src/test/java/jrodeud/springframework/sfpet/clinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jrodeud.springframework.sfpet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
