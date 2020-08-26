e8b3180310e3168d2ac1557745d0406803b0f18c
JRodEud <jrod_eudavepat@hotmail.com>
2020-08-26 13:35:04 -0500

    Impl Map based Services

---
+++ pet-clinic-data/src/main/java/jrodeud/springframework/sfpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package jrodeud.springframework.sfpetclinic.services.map;
+
+import jrodeud.springframework.sfpetclinic.model.Owner;
+import jrodeud.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deletedById(Long id) {
+        super.deletedById(id);
+    }
+}
